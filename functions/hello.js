15d98bfd619a1431e74db4aae0ec259c7fe176e6
1996Umesh <umesh123hirantha@gmail.com>
2025-07-12 17:45:27 +0530

    07-12

--- functions/hello.js
+++ functions/hello.js
@@ -1,33 +1,25 @@
+require('dotenv').config();
+
+const allowedOrigin = process.env.FRONTEND_URL || '*';
+
 exports.handler = async (event) => {
-    // Handle preflight request
     if (event.httpMethod === 'OPTIONS') {
-      return {
-        statusCode: 200,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Headers': 'Content-Type',
-          'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-        },
-        body: '',
-      };
+        return {
+            statusCode: 200,
+            headers: {
+                'Access-Control-Allow-Origin': allowedOrigin,
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+            },
+            body: '',
+        };
     }
-  
-    try {
-      return {
+
+    return {
         statusCode: 200,
         headers: {
-          'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Origin': allowedOrigin,
         },
         body: JSON.stringify({ message: "Hello from Netlify!" }),
-      };
-    } catch (error) {
-      return {
-        statusCode: 500,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-        },
-        body: JSON.stringify({ error: "Function error: " + error.message }),
-      };
-    }
-  };
-  
+    };
+};
