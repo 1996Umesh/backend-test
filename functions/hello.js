077f1f46b57b4e5e90e869cd6f52706dc13b5815
1996Umesh <umesh123hirantha@gmail.com>
2025-07-12 15:42:52 +0530

    07-12

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,23 @@
-exports.handler = async () => {
-    return {
+exports.handler = async (event) => {
+    if (event.httpMethod === 'OPTIONS') {
+      // Handle preflight
+      return {
         statusCode: 200,
-        body: JSON.stringify({ message: "Hello from Netlify!" })
+        headers: {
+          'Access-Control-Allow-Origin': 'https://f-end-test.netlify.app', // Or use your frontend domain
+          'Access-Control-Allow-Headers': 'Content-Type',
+          'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+        },
+        body: '',
+      };
+    }
+  
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': 'https://f-end-test.netlify.app', // Or 'https://f-end-test.netlify.app'
+      },
+      body: JSON.stringify({ message: "Hello from Netlify!" }),
     };
-};
+  };
+  
