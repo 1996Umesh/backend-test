8cd288dee2eb79b1d9ee599ab6b71bc3896a3215
1996Umesh <umesh123hirantha@gmail.com>
2025-07-19 09:28:01 +0530

    07-19

--- models/countrydirector.js
+++ models/countrydirector.js
@@ -2,34 +2,11 @@ const mongoose = require('mongoose');
 
 const CountryDirectorSchema = new mongoose.Schema(
     {
-        countrydirector_name: {
-            type: String,
-            required: true,
-            trim: true
-        },
-        country: {
-            type: String,
-            required: true,
-            trim: true
-        },
-        phone: {
-            type: String,
-            required: true,
-            trim: true
-        },
-        countrydirector_email: {
-            type: String,
-            required: true,
-            unique: true,
-            lowercase: true,
-            trim: true,
-            match: [/.+\@.+\..+/, 'Please fill a valid email address']
-        },
-        countrydirector_password: {
-            type: String,
-            required: true,
-            minlength: 6
-        }
+        country: { type: String, required: true },
+        countrydirector_name: { type: String, required: true },
+        phone: { type: String, required: true },
+        countrydirector_email: { type: String, required: true, unique: true },
+        countrydirector_password: { type: String, required: true }
     },
     {
         collection: 'countrydirector',
