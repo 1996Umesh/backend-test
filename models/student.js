10a3274b5760897dfa069bbdaec20da9128f4f19
1996Umesh <umesh123hirantha@gmail.com>
2025-07-27 13:00:39 +0530

    t

--- models/student.js
+++ models/student.js
@@ -20,13 +20,7 @@ const StudentSchema = new mongoose.Schema(
       required: true,
       trim: true,
     },
-    // student_country_code: {
-    //   type: String,
-    //   required: true,
-    //   trim: true,
-    //   maxlength: 5,
-    // },
-    student_phone_no: {
+    student_phone: {
       type: String,
       required: true,
       trim: true,
@@ -56,7 +50,7 @@ const StudentSchema = new mongoose.Schema(
       required: true,
       trim: true,
     },
-    guardian_phone_no: {
+    guardian_phone: {
       type: String,
       required: true,
       trim: true,
