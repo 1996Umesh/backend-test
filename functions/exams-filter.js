455a823e9bb358f5885c06219a9f9bd3d132f206
1996Umesh <umesh123hirantha@gmail.com>
2025-07-29 15:50:54 +0530

    t

--- functions/exams-filter.js
+++ functions/exams-filter.js
@@ -33,12 +33,46 @@ exports.handler = async (event) => {
 
     const filter = {};
 
-    if (country) filter.exam_country = country;
-    if (subject) filter.subject_id = subject;
-    if (examDate) filter.exam_date = examDate;
-    if (startTime) filter.exam_start_time = startTime;
+    // if (country) filter.exam_country = country;
+    // if (subject) filter.subject_id = subject;
+    // if (examDate) filter.exam_date = examDate;
+    // if (startTime) filter.exam_start_time = startTime;
 
-    const exams = await Exam.find(filter).sort({ exam_date: 1 }).lean();
+    // const exams = await Exam.find(filter).sort({ exam_date: 1 }).lean();
+
+    if (country && subject && examDate && startTime) {
+        const exams = await Exam.find({ country: country, subject_id:subject, exam_date: examDate, exam_start_time:startTime}).lean();
+    } else if (startTime) {
+        const exams = await Exam.find({ exam_start_time:startTime}).lean();
+    } else if (examDate) {
+        const exams = await Exam.find({ exam_date: examDate}).lean();
+    } else if (examDate && startTime) {
+        const exams = await Exam.find({ exam_date: examDate, exam_start_time:startTime}).lean();
+    } else if (subject) {
+        const exams = await Exam.find({ subject_id:subject}).lean();
+    } else if (subject && startTime) {
+        const exams = await Exam.find({ subject_id:subject, exam_start_time:startTime}).lean();
+    } else if (subject && examDate) {
+        const exams = await Exam.find({ subject_id:subject, exam_date: examDate}).lean();
+    } else if (subject && examDate && startTime) {
+        const exams = await Exam.find({ subject_id:subject, exam_date: examDate, exam_start_time:startTime}).lean();
+    } else if (country) {
+        const exams = await Exam.find({ country: country}).lean();
+    } else if (country && startTime) {
+        const exams = await Exam.find({ country: country, exam_start_time:startTime}).lean();
+    } else if (country && examDate) {
+        const exams = await Exam.find({ country: country, exam_date: examDate}).lean();
+    } else if (country && examDate && startTime) {
+        const exams = await Exam.find({ country: country, exam_date: examDate, exam_start_time:startTime}).lean();
+    } else if (country && subject) {
+        const exams = await Exam.find({ country: country, subject_id:subject}).lean();
+    } else if (country && subject && startTime) {
+        const exams = await Exam.find({ country: country, subject_id:subject, exam_start_time:startTime}).lean();
+    } else if (country && subject && examDate) {
+        const exams = await Exam.find({ country: country, subject_id:subject, exam_date: examDate}).lean();
+    } else {
+        const exams = '';
+    }
 
     return {
       statusCode: 200,
