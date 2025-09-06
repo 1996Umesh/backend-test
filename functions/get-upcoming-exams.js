35d8c6f4280d299534643415474cc3c863f49eab
1996Umesh <umesh123hirantha@gmail.com>
2025-09-06 09:31:50 +0530

    09-06

--- functions/get-upcoming-exams.js
+++ functions/get-upcoming-exams.js
@@ -17,7 +17,7 @@ const headers = {
 //     'Content-Type': 'application/json',
 // };
 
-exports.handler = async (event) => {
+exports.handler = async (event, context) => {
     if (event.httpMethod === 'OPTIONS') {
         return {
             statusCode: 200,
